a6f254482467d03a560cd40ce62429ea070eb2e9
Syed Zawad <syedzawadd@gmail.com>
2022-11-12 02:12:21 +0600

    mongodb database connected

--- index.js
+++ index.js
@@ -18,9 +18,18 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster-01.qwgvzig.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
 
+    }
+    finally {
 
+    }
+}
 
+run().catch(error => {
+    console.log(error);
+})
 
 
 app.get('/', (req, res) => {
