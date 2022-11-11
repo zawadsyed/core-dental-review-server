9ae2432f13e9a7e051e4a24d957e024be277a394
Syed Zawad <syedzawadd@gmail.com>
2022-11-12 02:09:43 +0600

    mongodb server connected

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster-01.qwgvzig.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 
 app.get('/', (req, res) => {
