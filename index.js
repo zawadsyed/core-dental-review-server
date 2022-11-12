1e9d2ce734b06d0c91cbe68661bce9728e73a2bc
Syed Zawad <syedzawadd@gmail.com>
2022-11-12 18:51:54 +0600

    services get api created

--- index.js
+++ index.js
@@ -20,7 +20,14 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-
+        const serviceCollection = client.db('coreDentalReview').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            console.log(services)
+            res.send(services);
+        })
     }
     finally {
 
